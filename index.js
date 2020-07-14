cc166adb96991a22ce3886c29d7742890fa1560f
Daniel Park <ekklesia89@gmail.com>
2020-07-14 11:47:56 +0900

    stock list and code xls added and xls to json list

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const parseString = require("xml2js").parseString;
+const XLSX = require("xlsx");
 
 const PORT = process.env.NODE_PORT || 5000;
 
@@ -16,8 +17,22 @@ app.get("/", async (req, res, err) => {
   );
   parseString(data.data, (err, result) => {
     let body = result.stockprice.TBL_StockInfo[0].$;
-    res.send(body.CurJuka);
+    res.send(body);
   });
+});
+
+app.get("/code", async (req, res, err) => {
+  let workbook = XLSX.readFile("./assets/code.xls");
+  let sheet = workbook.Sheets.Sheet1;
+
+  let result = XLSX.utils.sheet_to_json(sheet).map((v) => ({
+    code: v["종목코드"],
+    name: v["종목명"],
+  }));
+
+  console.log(result);
+
+  res.send(result);
 });
 
 app.listen(PORT, () => {
