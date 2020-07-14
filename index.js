32eb9e1cc5927c4bfae04f87f9fb3ce515a87613
Daniel Park <ekklesia89@gmail.com>
2020-07-14 17:18:14 +0900

    naver juka info api call added

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req, res, err) => {
-  const data = await axios.get(
-    `http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code=${req.query.code}`
-  );
-  parseString(data.data, (err, result) => {
-    let body = result.stockprice.TBL_StockInfo[0].$;
-    res.send(body);
-  });
-});
+// app.get("/", async (req, res, err) => {
+//   const data = await axios.get(
+//     `http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code=${req.query.code}`
+//   );
+//   parseString(data.data, (err, result) => {
+//     let body = result.stockprice.TBL_StockInfo[0].$;
+
+//     console.log(result.stockprice.TBL_StockInfo);
+//     res.send(body);
+//   });
+// });
 
 app.get("/code", async (req, res, err) => {
   let workbook = XLSX.readFile("./assets/code.xls");
@@ -30,9 +32,28 @@ app.get("/code", async (req, res, err) => {
     name: v["종목명"],
   }));
 
-  console.log(result);
-
   res.send(result);
+});
+
+app.get("/", async (req, res, err) => {
+  let url = "https://api.finance.naver.com/service/itemSummary.nhn?itemcode=";
+  const body = await axios.get(url + req.query.code);
+
+  res.send(body.data);
+  // {
+  //   "marketSum":221817, -> 시가 총액  2218억 17백만
+  //   "risefall":2,    -> 1 : 상한 , 2 : 상승, 3: 보합? , 4 : 하한, 5, 하락
+  //   "diff":3200,    -> 전일대비 가격 차이
+  //   "rate":9.51,  -> 상승율
+  //   "high":38900, ->고가
+  //   "low":34150, ->저가
+  //   "quant":329660, -> 거래량
+  //   "amount":12156468, -> 거래대금
+  //   "per":24.31, -> PER
+  //   "eps":1516.0, -> EPS
+  //   "pbr":4.25, -> PBR
+  //   "now":36850  -> 현재가
+  //  }
 });
 
 app.listen(PORT, () => {
